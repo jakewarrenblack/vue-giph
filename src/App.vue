9eaf175ba8e04643a7c3e8393d78603ab441cd20
Jake Black (Student) <N00193326@iadt.ie>
2021-10-07 12:35:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+ 
+  </div>
+</template>
+
+<script>
+import GiphyViewer from '@/components/GiphyViewer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // equivalent to 'GiphyViewer = GiphyViewer'
+    // js just instantiates this way
+    GiphyViewer
+ 
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/GiphyViewer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        This is my GiphyViewer component
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'GiphyViewer',
+        data() {
+            // data needs to return an object
+            return {
+
+            };
+        },
+        methods: {
+            // search method will go in here
+        }
+    }
+</script>
+
+<style>
+</style>
